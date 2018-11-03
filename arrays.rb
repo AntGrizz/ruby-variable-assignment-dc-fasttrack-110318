5b52afb28daa4f06b7a3aa8c2e09b0999f47e4bb
Anthony Gregg <agregg22@gmail.com>
2018-11-03 15:54:21 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,9 @@
-student1 = 25
-student2 = 35
-student3 = 32
-
-puts student1
-puts student2
-puts student3
+# student1 = 25
+# student2 = 35
+# student3 = 32
+#
+# puts student1
+# puts student2
+# puts student3
+studentages [25, 32, 33]
+puts studentages
