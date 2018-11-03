58bb2a10c8b95f02f42d4391acdc4f4ff9425cb5
Anthony Gregg <agregg22@gmail.com>
2018-11-03 15:35:40 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,8 +1,11 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-greeting = "Hello World"
-h = "hello World"
-r = 2
-
-bool = true
-puts bool
+# greeting = "Hello World"
+# h = "hello World"
+# r = 2
+#
+# bool = true
+# puts bool
+num1 = 1
+num2 = 2
+newnum = num1 + num2
