d8e949393bffa6a40d7ed315d2b1dfb96ee54082
Anthony Gregg <agregg22@gmail.com>
2018-11-03 15:57:37 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -1,9 +1,15 @@
-# student1 = 25
-# student2 = 35
-# student3 = 32
-#
-# puts student1
-# puts student2
-# puts student3
-studentages = [25, 32, 33]
-puts studentages
+# # student1 = 25
+# # student2 = 35
+# # student3 = 32
+# #
+# # puts student1
+# # puts student2
+# # puts student3
+# studentages = [25, 32, 33]
+# puts studentages
+def printstudentnames
+  studentnames = ["Ann", "Jakes", "Josh"]
+  puts studentnames
+end
+
+printstudentnames
